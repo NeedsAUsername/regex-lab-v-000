5ee27ec050ff4c9bef3bc65a23de8d279f8d31a0
Kevin Chi <kevinchi118@gmail.com>
2018-06-11 22:20:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,6 @@
 def starts_with_a_vowel?(word)
-    if word.match(/^[aeiouAEIOU]/)
-        true
-    else false
-    end
+    word.match(/^[aeiouAEIOU]/) ? true : false
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -14,15 +12,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    if text.match(/^[A-Z].*[.?!]$/)
-        true
-    else false
-    end
+    text.match(/^[A-Z].*[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-    if phone.match(/[\d]{3}-*[\d]{3}-*[\d]{4}/)
-        true
-    else false
-    end
+    phone.match(/\d{10}/) ? true : false
+
 end
