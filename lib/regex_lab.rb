bc60164c7f8ea00c4e248ab7697f3bcc99240b16
Kevin Chi <kevinchi118@gmail.com>
2018-06-11 22:09:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,10 @@ end
 
 def words_five_letters_long(text)
     text.scan(/\b[a-zA-z]{5}\b/)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.scan(/\b[A-Z]\w+[./?!]\b/)
 end
 
 def valid_phone_number?(phone)
